ee84d30d9e8886d621a8897b3bc2d9f5d8c444e4
Woo Jin Kim <kimwoojin211@gmail.com>
2021-03-05 11:04:38 -0800

    add and pass vendor GetId() test

--- Tracker.Tests/ModelTests/VendorTests.cs
+++ Tracker.Tests/ModelTests/VendorTests.cs
@@ -6,19 +6,23 @@ using Tracker.Models;
 namespace Tracker.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
     [TestMethod]
     public void VendorConstructor_CreatesInstanceofVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("a","b");
+      Vendor newVendor = new Vendor("Test","Test");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
     [TestMethod]
     public void GetName_ReturnsName_String()
     {
-      string name = "Vendor 1";
-      string description = "Vendor 1 Description";
+      string name = "Test vendor";
+      string description = "Test vendor description";
       Vendor newVendor = new Vendor(name,description);
       string result = newVendor.Name;
       Assert.AreEqual(name,result);
@@ -26,11 +30,22 @@ namespace Tracker.Tests
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
-      string name = "Vendor 1";
-      string description = "Vendor 1 Description";
+      string name = "Test vendor";
+      string description = "Test vendor description";
       Vendor newVendor = new Vendor(name,description);
       string result = newVendor.Description;
       Assert.AreEqual(description,result);
     }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      string name = "Test vendor";
+      string description = "Test vendor Description";
+      Vendor newVendor = new Vendor(name,description);
+      int result = newVendor.Id;
+      Assert.AreEqual(1, result);
+    }
+
   }
 }
