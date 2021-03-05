e2781faec5818063079714dc07df562c655ca142
Woo Jin Kim <kimwoojin211@gmail.com>
2021-03-05 13:58:52 -0800

    build page to register new orders

--- Tracker/Controllers/OrdersController.cs
+++ Tracker/Controllers/OrdersController.cs
@@ -1,5 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using Tracker.Models;
 
 namespace Tracker.Controllers
 {
+  public class OrdersController: Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor newVendor = Vendor.Find(vendorId);
+      return View(newVendor);
+    }
+  }
 }
